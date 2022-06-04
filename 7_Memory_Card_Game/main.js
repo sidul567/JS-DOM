c4b6b8f7333032dce90874d8d1c62e96e1954489
Sidul Islam Moon <56472753+sidul567@users.noreply.github.com>
2022-06-04 13:03:26 +0600

    first message

--- 7_Memory_Card_Game/main.js
+++ 7_Memory_Card_Game/main.js
@@ -1,12 +1,47 @@
 let cards = document.querySelectorAll('.card');
+let time = document.querySelector('.details .time');
+let flip = document.querySelector('.details .flips');
+let refreshBtn = document.querySelector('.details button');
 let disableFlip = false;
 let matchCountCard = 0;
+let flipCount = 0;
+let timeCount = 60;
+let isTimeEnd = false;
+let timer = null;
+let isWatch = false;
 
 let cardOne, cardTwo;
+time.innerHTML = `Time: ${timeCount}s`;
+function timeChange(){
+    timeCount--;
+    if(timeCount < 0){
+        isTimeEnd = true;
+        clearInterval(timer);
+        cards.forEach(card=>card.removeEventListener('click',flipCard));
+    }else{
+        time.innerHTML = `Time: ${timeCount < 10? '0'+timeCount: timeCount}s`;
+    }
+}
+
+refreshBtn.addEventListener('click',()=>{
+    timeCount = 60;
+    time.innerHTML = `Time: ${timeCount}s`;
+    isTimeEnd = false;
+    isWatch = false;
+    flipCount = 0;
+    flip.innerHTML = `Flips: ${flipCount}`;
+    shuffleCard();
+})
 
 function flipCard(e){
     let clickElement = e.target;
     if(clickElement !== cardOne && !disableFlip){
+        if(!isTimeEnd && !isWatch){
+            timer = setInterval(timeChange,1000);
+            isWatch = true;
+        }
+        flipCount++;
+        flip.innerHTML = `Flips: ${flipCount}`;
         clickElement.classList.add('flip');
         if(!cardOne){
             return cardOne = clickElement;
@@ -50,14 +85,12 @@ function shuffleCard(){
     let arr = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8];
     arr.sort((a,b)=>Math.random() > 0.5 ? a-b : b-a);
     cards.forEach((card,index)=>{
-        card.classList.remove('flip');
-        card.addEventListener('click',flipCard);
         card.querySelector('.back-view img').src = `img/img-${arr[index]}.png`;
+        card.classList.add('flip');
+        setTimeout(() => {
+            card.classList.remove('flip');
+            card.addEventListener('click',flipCard);
+        }, 1500);
     })
 }
 shuffleCard();
-
-cards.forEach(card=>{
-    // card.classList.add('flip');
-    card.addEventListener('click',flipCard);
-})
